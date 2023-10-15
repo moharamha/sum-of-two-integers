4b58741585c1b3eb7c3c808cf1dc3364e0307edf
moharamha <147977068+moharamha@users.noreply.github.com>
2023-10-15 13:02:58 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -2,13 +2,13 @@
 #include <bits/stdc++.h>
 using el = "\n";
 using namespace std;
-int add(int a,int b)
+int add2(int a,int b)
 {
 	return a + b;
 }
 int main()
 {
 	int a, b; cin >> a >> b;
-	cout << add(a, b)<<el;
+	cout << add2(a, b)<<el;
 
 }
