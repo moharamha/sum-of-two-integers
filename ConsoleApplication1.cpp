d579fe442f761b034036e2de412acc5bdd2c3512
moharamha <147977068+moharamha@users.noreply.github.com>
2023-10-15 12:59:54 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using el = "\n";
+using namespace std;
+int add(a, b)
+{
+	return a + b;
+}
+int main()
+{
+	int a, b; cin >> a >> b;
+	cout << add(a, b)<<el;
+
+}
